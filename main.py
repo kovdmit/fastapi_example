13d31e34fcb0283ec705a15af5868828bdb35c9d
Dmitry Kovalev <pzhabbiu@gmail.com>
2023-08-02 09:59:09 +0300

    Body - Вложенные модели

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
+
+
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 
 class Item(BaseModel):
@@ -10,9 +15,16 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
     tags: set[str] = set()
+    images: list[Image] | None = None
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
-    return results
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[Item]
+
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer
