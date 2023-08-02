eb98ed0cc158a926d5c56b564a83fc2c52421b3a
Dmitry Kovalev <pzhabbiu@gmail.com>
2023-08-02 11:02:54 +0300

    Модель ответа - Возвращаемый тип

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class BaseUser(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str | None = None
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float = 10.5
+    tags: list[str] = []
 
 
-class UserIn(BaseUser):
-    password: str
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
 
 
-@app.post("/user/")
-async def create_user(user: UserIn) -> BaseUser:
-    return user
+@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
