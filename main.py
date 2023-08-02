414a2cc196689dc519a2260302800f0ffb756476
Dmitry Kovalev <pzhabbiu@gmail.com>
2023-08-02 10:55:58 +0300

    Модель ответа - Возвращаемый тип

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-from datetime import datetime, time, timedelta
-from typing import Union
-from uuid import UUID
-
-from fastapi import Body, FastAPI
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
 
-@app.put("/items/{item_id}")
-async def read_items(
-    item_id: UUID,
-    start_datetime: Union[datetime, None] = Body(default=None),
-    end_datetime: Union[datetime, None] = Body(default=None),
-    repeat_at: Union[time, None] = Body(default=None),
-    process_after: Union[timedelta, None] = Body(default=None),
-):
-    start_process = start_datetime + process_after
-    duration = end_datetime - start_process
-    return {
-        "item_id": item_id,
-        "start_datetime": start_datetime,
-        "end_datetime": end_datetime,
-        "repeat_at": repeat_at,
-        "process_after": process_after,
-        "start_process": start_process,
-        "duration": duration,
-    }
+class BaseUser(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserIn(BaseUser):
+    password: str
+
+
+@app.post("/user/")
+async def create_user(user: UserIn) -> BaseUser:
+    return user
